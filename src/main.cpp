5cdbfd02678399a3ae740c20ad17eae58174f17e
Allison Zhao <allisonzhao.uni@gmail.com>
2025-02-13 13:05:04 -0500

    print date and time in serial terminal with ntp server (connect through wifi)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// Getting date and time from NTP server: https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
+
 #include <WiFi.h>
 #include "time.h"
 
@@ -7,38 +9,70 @@ const char* ssid       = "";  // insert wifi name here
 const char* password   = "";  // insert wifi password here
 
 const char* ntpServer = "pool.ntp.org";  // want to connect to this server to get time
-const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 3600;
+const long  gmtOffset_sec = -5 * 3600;  // EST is UTC-5
+const int   daylightOffset_sec = 3600;  // Daylight Saving Time offset (1 hour)
 
-// variable definitions go up here
+// variable and function definitions go up here
 bool isConnected = false;
+void printLocalTime();
 
 void setup()
 {
   Serial.begin(9600);
 
-  pinMode(LED, OUTPUT);
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
   
-  Serial.println("starting");
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+
+
 }
 
 void loop()
 {
-  // ESP32 is connected to wifi
-  if (WiFi.status() == WL_CONNECTED && !isConnected) {
-    Serial.println("Connected");
-    digitalWrite(LED, HIGH);
-    isConnected = true;
-    
+  delay(1000);
+  printLocalTime();
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
   }
 
-  // ESP32 is not connected to wifi
-  if (WiFi.status() != WL_CONNECTED){
-    Serial.println(".");
-    digitalWrite(LED, !digitalRead(LED));
-    delay(1000);
-    isConnected = false;
-  }
+  // print date, day of the week, month, day, year, hour, minute, and second in serial terminal
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println();
 }
