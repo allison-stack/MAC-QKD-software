b7789f03b6b925d41b179ad028bdafb56455544f
Allison Zhao <allisonzhao.uni@gmail.com>
2025-03-06 21:54:57 -0500

    add skeleton code for Alice's side of the protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int BlueLedPin = 2;
+// Alice Skeleton Code
 
-void LaserPulse(int pin, int HighTime, int LowTime){
-  digitalWrite(pin, HIGH);
-  delay(HighTime);
-  digitalWrite(pin, LOW);
-  delay(LowTime);
-}
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting Program");
-  pinMode(BlueLedPin, OUTPUT);
-  Serial.println("Pin Configured");
+  // time syncronizatoin with bob
+    // wifi connect to server get time (turn wifi off when done)
+
+  // determine key length
+    // account for the fact that they will loose some bits so add approx 50% more bits to basis selection and bits sent
+
+  // determine basis slection at begining and store in array
+    // X amount of random basis
+    // this will save clock cycles in the loop where we are sending bits
+
+  // need to decide whether this only happens once or the key generation is a continuos process
+  
+  // loop with counter for X number of bits 
+    // use the random basis array to change to the basis at the specified index 
+    // send a pulse
+    // record the time that the pulse was sent and put that with the corresponding basis index
+    // possibly add delay to ensure that Bob has enough time to recieve the bit
+
+  // send data to Bob
+    // array of basis and timestamps
+  
+  // recive Bobs data
+    // compare the timestamps to ensure the bit sent is the same bit recieved
+    // if the bits are the same compare basis selection
+      // if basis are same keep bit for the key (if not discard the bit)
+
+  
 }
 
+
 void loop() {
-  LaserPulse(BlueLedPin, 10, 7000);
+
 }
