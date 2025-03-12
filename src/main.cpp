064a0f7b60e44f4d0b602ab895b6fa81d732f9d9
Allison Zhao <allisonzhao.uni@gmail.com>
2025-03-12 19:19:14 -0400

    code for Alice's side of the protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-// Getting date and time from NTP server: https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
-
+#include <Arduino.h>
 #include <WiFi.h>
 #include "time.h"
-
 #define LED 2
 
 const char* ssid       = "";  // insert wifi name here
@@ -14,13 +12,22 @@ const int   daylightOffset_sec = 3600;  // Daylight Saving Time offset (1 hour)
 
 // variable and function definitions go up here
 bool isConnected = false;
-void printLocalTime();
+int keyLength = 32;
+int aliceBasisSelection[32];  // Array to store basis selections
+unsigned long aliceTimestamps[32];  // Array to store timestamps
 
-void setup()
-{
+void printLocalTime();
+void generateBasisSelection();
+void sendBits();
+void receiveDataFromBob();
+
+// Alice's side
+
+void setup() {
+  // time syncronization with bob
   Serial.begin(9600);
 
-  // Connect to Wi-Fi
+  // connect to Wi-Fi
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
@@ -33,31 +40,46 @@ void setup()
   Serial.println("");
   Serial.println("WiFi connected.");
   
-  // Init and get the time
+  // initialize and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   printLocalTime();
 
-  //disconnect WiFi as it's no longer needed
+  //disconnect wifi as it's no longer needed
   WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
 
+  // generate random basis selection
+  generateBasisSelection();
 
+  // send bits to Bob
+  sendBits();
+
+  // receive data from Bob and compare
+  receiveDataFromBob();
+
+  // determine key length
+    // account for the fact that they will loose some bits so add approx 50% more bits to basis selection and bits sent
+
+  // send data to Bob
+    // array of basis and timestamps
+  
+  // recive Bobs data
+    // compare the timestamps to ensure the bit sent is the same bit recieved
+    // if the bits are the same compare basis selection
+      // if basis are same keep bit for the key (if not discard the bit)
 }
 
-void loop()
-{
-  delay(1000);
-  printLocalTime();
+void loop() {
+  // empty
 }
 
+// print current time
 void printLocalTime(){
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
   }
-
-  // print date, day of the week, month, day, year, hour, minute, and second in serial terminal
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
   Serial.print("Day of week: ");
   Serial.println(&timeinfo, "%A");
@@ -73,6 +95,64 @@ void printLocalTime(){
   Serial.println(&timeinfo, "%M");
   Serial.print("Second: ");
   Serial.println(&timeinfo, "%S");
-
   Serial.println();
 }
+
+// determine basis slection at begining and store in array
+void generateBasisSelection() {
+  for (int i = 0; i < keyLength; i++) {
+    // Random basis (either 0 or 1)
+    aliceBasisSelection[i] = esp_random() & 0x01;
+  }
+}
+
+// send X number of bits using the random basis array
+void sendBits() {
+  for (int i = 0; i < keyLength; i++) {
+    // set the wave plate according to the basis selection
+    Serial.print("Setting wave plate to basis: ");
+    Serial.println(aliceBasisSelection[i]);
+
+    // send pulse
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+
+    // record timestamp of pulse
+    aliceTimestamps[i] = millis();
+
+    // ensure enough delay in between pulses
+    delay(500);
+  }
+}
+
+void receiveDataFromBob() {
+  int bobBasisSelection[32];
+  unsigned long bobTimestamps[32];
+  // Bob's basis selection and timestamps
+  for (int i = 0; i < keyLength; i++) {
+    bobBasisSelection[i] = esp_random() & 0x01;
+  }
+  // compare timestamps and basis selections
+  for (int i = 0; i < keyLength; i++) {
+    if (abs((long)(aliceTimestamps[i] - bobTimestamps[i])) < 50) {
+      if (aliceBasisSelection[i] == bobBasisSelection[i]) {
+        Serial.print("Bit ");
+        Serial.print(i);
+        Serial.println(" is valid and can be used for the key.");
+      } else {
+        Serial.print("Bit ");
+        Serial.print(i);
+        Serial.println(" has different basis selections and will be discarded.");
+      }
+    } else {
+      Serial.print("Bit ");
+      Serial.print(i);
+      Serial.println(" has too large a time difference and will be discarded.");
+    }
+  }
+}
+
+
+
+
